e6bf43851c9ce1fad1bcce2204614913578f0f8e
jaronchan <jaron.chan.jin.jia@gmail.com>
2018-07-11 14:22:10 +0800

    implement organisational structure tab in our-team

--- assets/js/our-team.js
+++ assets/js/our-team.js
@@ -3,8 +3,10 @@ document.getElementById("senior-leadership-button").addEventListener("click", di
 function displaySeniorLeadership() {
 	document.getElementById("board-of-directors").style.display = "none";
 	document.getElementById("senior-leadership").style.display = "flex";
+	document.getElementById("organisational-structure").style.display = "none";
 	document.getElementById("board-of-directors-button").classList.remove("selected");
 	document.getElementById("senior-leadership-button").classList.add("selected");
+	document.getElementById("organisational-structure-button").classList.remove("selected");
 }
 
 document.getElementById("board-of-directors-button").addEventListener("click", displayBoard);
@@ -12,6 +14,19 @@ document.getElementById("board-of-directors-button").addEventListener("click", d
 function displayBoard() {
 	document.getElementById("board-of-directors").style.display = "flex";
 	document.getElementById("senior-leadership").style.display = "none";
+	document.getElementById("organisational-structure").style.display = "none";
 	document.getElementById("board-of-directors-button").classList.add("selected");
 	document.getElementById("senior-leadership-button").classList.remove("selected");
+	document.getElementById("organisational-structure-button").classList.remove("selected");
+}
+
+document.getElementById("organisational-structure-button").addEventListener("click", displayOrganisation);
+
+function displayOrganisation() {
+	document.getElementById("board-of-directors").style.display = "none";
+	document.getElementById("senior-leadership").style.display = "none";
+	document.getElementById("organisational-structure").style.display = "block";
+	document.getElementById("board-of-directors-button").classList.remove("selected");
+	document.getElementById("senior-leadership-button").classList.remove("selected");
+	document.getElementById("organisational-structure-button").classList.add("selected");
 }
